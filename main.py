c23edeeb728eff58aa1fe9827af046c857c18727
fullstack-sake <64351788+fullstack-sake@users.noreply.github.com>
2021-12-05 12:36:47 +0800

    修改了配速计算方式

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ class LegymPost:
                 "totalPart": 0.0,
                 "type": "自由跑",
                 "uneffectiveReason": "",
-                "avePace": random_time / distance * 1000 + random.randint(0,1) / 10,
+                "avePace": random_time * 60/ distance * 1000 + random.randint(0,1) / 10,
                 "calorie": int(distance * random.uniform(70.0,75.0)),
                 "effectiveMileage": distance,
                 "effectivePart": 1,
