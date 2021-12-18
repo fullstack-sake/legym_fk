b8321c979b5fb572bc2fea9aca44712347228137
MrCai <1014305148@qq.com>
2021-12-18 17:07:41 +0800

    :sparkles: 实现活动查询

--- src/cli.py
+++ src/cli.py
@@ -1,16 +1,14 @@
 import os
 
-from legym import LegymHacker
+from legym import *
 
 
 class LegymCLI(LegymHacker):
     def __init__(self) -> None:
         super().__init__()
-        self.__show_welcome()
-
-    def __show_welcome(self) -> None:
-        """Show welcome to user at the beginning."""
         print("------ 欢迎使用乐健命令行 ------")
+        while not self.__cli_login():
+            pass
         print("如果您是初次使用该应用，可以输入 `help` 来查看各命令使用方法。")
 
     def __valid_input(self, prompt: str = ">>> ") -> str:
@@ -27,43 +25,46 @@ class LegymCLI(LegymHacker):
             user_input = input(prompt)
         return user_input
 
+    def __cli_login(self) -> bool:
+        """Show welcome to user at the beginning.
+
+        Returns:
+            `True` on success, or `False` on failure.
+        """
+        username = self.__valid_input("用户名：")
+        password = self.__valid_input("密码：")
+        try:
+            name, school = self.login(username, password)
+        except LegymException as e:
+            print(e)
+            return False
+        else:
+            print(f"{school}的{name}，欢迎回来！")
+            return True
+
     def run(self) -> None:
         """Run CLI application."""
         while True:
-            commands = self.__valid_input().split()
-            if commands[0] == "login":
-                self.__parse_login(commands[1:])
-            elif commands[0] == "reg":
-                self.__parse_reg(commands[1:])
-            elif commands[0] == "sign":
-                self.__parse_sign(commands[1:])
-            elif commands[0] == "act":
-                self.__parse_act(commands[1:])
-            elif commands[0] == "help":
-                self.__parse_help(commands[1:])
-            elif commands[0] == "quit":
-                self.__parse_quit()
-            else:
-                print(f"不支持的命令：{commands[0]}")
-
-    def __parse_login(self, args: list) -> None:
-        """Parse login command.
-
-        Args:
-            args: List containing username and password.
-        """
-        if len(args) == 0:
-            username = self.__valid_input("用户名：")
-            password = self.__valid_input("密码：")
-        elif len(args) == 1:
-            username = args[0]
-            password = self.__valid_input("密码：")
-        else:
-            username = args[0]
-            password = args[1]
-
-        name, school = self.login(username, password)
-        print(f"{school}的{name}，欢迎回来！")
+            try:
+                commands = self.__valid_input().split()
+                if commands[0] == "login":
+                    self.__parse_login(commands[1:])
+                elif commands[0] == "reg":
+                    self.__parse_reg(commands[1:])
+                elif commands[0] == "sign":
+                    self.__parse_sign(commands[1:])
+                elif commands[0] == "act":
+                    self.__parse_act(commands[1:])
+                elif commands[0] == "run":
+                    self.__parse_run(commands[1:])
+                elif commands[0] == "help":
+                    self.__parse_help(commands[1:])
+                elif commands[0] == "quit":
+                    self.__parse_quit()
+                else:
+                    print(f"不支持的命令：{commands[0]}")
+            except LegymException as e:
+                print(e)
 
     def __parse_reg(self, args: list) -> None:
         """Parse help command.
@@ -77,6 +78,32 @@ class LegymCLI(LegymHacker):
         pass
 
     def __parse_act(self, args: list) -> None:
+        """Parse activity command.
+
+        Args:
+            args: List containing activity category.
+        """
+        if len(args) == 0:
+            self.__show_all_activities()
+        else:
+            category = args[0]
+            state = (
+                ActivityState.SIGNED
+                if category == "signed"
+                else ActivityState.REGISTERED
+                if category == "registered"
+                else ActivityState.AVAILABLE
+                if category == "available"
+                else ActivityState.BLOCKED
+                if category == "blocked"
+                else -1
+            )
+            if state == -1:
+                print(f"类别不存在：{category}")
+            else:
+                self.__show_specified_activities(state)
+
+    def __parse_run(self, args: list) -> None:
         pass
 
     def __parse_help(self, args: list) -> None:
@@ -97,6 +124,28 @@ class LegymCLI(LegymHacker):
         """Parse quit command."""
         print("------ 感谢支持 ------\n")
         exit(0)
+
+    def __show_all_activities(self) -> None:
+        """Show all activities."""
+        print("已签到：")
+        self.__show_specified_activities(ActivityState.SIGNED)
+        print("\n已报名：")
+        self.__show_specified_activities(ActivityState.REGISTERED)
+        print("\n可报名：")
+        self.__show_specified_activities(ActivityState.AVAILABLE)
+        print("\n未开始：")
+        self.__show_specified_activities(ActivityState.BLOCKED)
+
+    def __show_specified_activities(self, state: int) -> None:
+        """Show activities of specified state.
+
+        Args:
+            Specified state.
+        """
+        activities = list(
+            filter(lambda activity: activity["state"] == state, self._activities)
+        )
+        print("\n".join([activity["name"] for activity in activities]))
 
 
 if __name__ == "__main__":
