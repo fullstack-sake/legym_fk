9ea306c95060fa0d3be0f11e138694c98970e17c
MrCai <1014305148@qq.com>
2021-12-22 11:37:57 +0800

    :fire: 清除 legym 模块代码

--- src/legym/__init__.py
+++
@@ -1,6 +0,0 @@
-from .activities import LegymActivities
-from .activity import ActivityState, LegymActivity
-from .core import Legym
-from .exception import LegymException
-from .requester import LegymRequester
-from .response import LegymResponse
